676b1efed09ed2ba742be29c5d7340856465fe5b
Yulia Semenyuk <semenyukyp@gmail.com>
2023-03-31 22:20:48 +0300

    get posts

--- blog-app/app/layout.tsx
+++ blog-app/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <nav>
           <h1>Navbar</h1>
         </nav>
---
+++ blog-app/lib/getUser.tsx
@@ -0,0 +1,7 @@
+export default async function getUser(userId: number) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+
+    if (!res.ok) throw new Error('Failed to fetch user')
+
+    return res.json()
+}
