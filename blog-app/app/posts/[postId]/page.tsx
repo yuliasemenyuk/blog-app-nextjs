72ecccd9f8e6b99735ccfa3ded490eb136d97663
Yulia Semenyuk <semenyukyp@gmail.com>
2023-04-05 19:26:05 +0300

    change post to ssg

--- blog-app/app/posts/[postId]/page.tsx
+++ blog-app/app/posts/[postId]/page.tsx
@@ -3,6 +3,14 @@ import { getPostData, getSortedPostsData } from " @component/lib/posts"
 import { notFound } from "next/navigation";
 import Link from "next/link";
 
+export function generateStaticParams() {
+    const posts = getSortedPostsData()
+
+    return posts.map(post => ({
+        postId: post.id
+    }))
+}
+
 export function generateMetadata({ params }: { params: { postId: string } }) {
 
     const posts = getSortedPostsData()
