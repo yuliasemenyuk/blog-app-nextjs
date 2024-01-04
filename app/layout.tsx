114e2a1d8078e072247291f05acdc60e8eb3cd69
YulliaAC <yul@appsclicks.com>
2024-01-04 13:19:49 +0200

    add GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,48 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google'
-import Navbar from './components/Navbar';
-import MyProfilePic from './components/MyProfilePic';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Navbar from "./components/Navbar";
+import MyProfilePic from "./components/MyProfilePic";
+import Script from "next/script";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Blog',
-  description: 'Created with next.js',
-}
-
+  title: "Blog",
+  description: "Created with next.js",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className='dark:bg-slate-800'>
+      {/* <!-- Google Tag Manager --> */}
+      <Script id="gtm-script">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KH56F54Q');`}
+      </Script>
+      {/* <!-- End Google Tag Manager --> */}
+      <body className="dark:bg-slate-800">
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KH56F54Q"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         <Navbar />
         <MyProfilePic />
         {children}
-        </body>
+      </body>
     </html>
-  )
+  );
 }
